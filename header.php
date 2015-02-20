7f39da4155bda2fd6bb9664440382291108412aa
gisselmann <flythe747@free.fr>
2015-02-20 20:01:35 +0100

    modif du css .home en .utilisateur suite changement nom bloc div

--- header.php
+++ header.php
@@ -81,7 +81,7 @@
                         <div class="row">
                             <div class="col-md-3">
                                 <div class="logo">
-                                    <a href="#"><img src="images/logo.png" title="Grill Template" alt="Grill Website Template" ></a>
+                                    <a href="#"><img src="images/logo.png" title="Grill" alt="logo de loic et bertrand" ></a>
                                 </div>
                             </div>
                             <div class="col-md-6">
@@ -90,7 +90,7 @@
                                         <li><a href="index.php">Accueil</a></li>
                                         <li><a href="produits.php">Produits</a></li>
 										<li><a href="panier.php">Mon panier</a></li>
-                                        <li><a href="contact.html">Nous contacter</a></li>
+                                        <li><a href="contact.php">Nous contacter</a></li>
                                     </ul>
                                 </div>
                             </div>
