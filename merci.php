bc9d7dae9be6dad0d8fe6d8fda9bb325c7b1e21b
gisselmann <flythe747@free.fr>
2015-02-21 23:50:09 +0100

    changement complet de la page paiement.php page merci.php correspondra a la validation de la commande

---
+++ merci.php
@@ -0,0 +1,23 @@
+<?php include_once("header.php");//viderPanier();// ?>
+
+
+
+
+<h1>merci!!!!</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include_once("footer.php") ?>
--- paiement.php
+++ paiement.php
@@ -1,28 +1,75 @@
-<?php
+<?php include_once("header.php"); ?>
+<div class="container">
+    <div class='row'>
+        <div class='col-md-4'></div>
+        <div class='col-md-4'>
+          <script src='https://js.stripe.com/v2/' type='text/javascript'></script>
+          
+		  
+		  <form accept-charset="UTF-8" action="merci.php" class="require-validation" data-cc-on-file="false" 
+		  data-stripe-publishable-key="pk_bQQaTxnaZlzv4FnnuZ28LFHccVSaj" id="payment-form" 
+		  method="post">
+		  
+		  <div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="✓" />
+			<input name="_method" type="hidden" value="PUT" /><input name="authenticity_token" 
+			type="hidden" value="qLZ9cScer7ZxqulsUWazw4x3cSEzv899SP/7ThPCOV8=" />
+		  </div>
+           
 
-  $email_paypal= 'RestoRap';/*email associé au compte paypal du vendeur*/
-  $item_numero = '04AAA'; /*Numéro du produit en vente*/
-  $item_prix   = '40';    /*prix du produit*/
-  $item_nom    = 'Pizza 4 fromages'; /*Nom du produit*/
-  $url_retour='http://www.memo-web.fr/paypal-remerciement.php';/*page de remerciement à créer*/
-  $url_cancel='http://www.memo-web.fr/paypal-annulation.php'; /*page d'annulation d'achat*/
-  $url_confirmation='http://www.memo-web.net/paypal-confirmation.php';/*page de confirmation d'achat*/
-/* fin déclaration des variables */
- 
-  echo '
-  <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-  <input type="hidden" name="cmd" value="_xclick"/>
-  <input type="hidden" name="business" value="'.$email_paypal.'"/>
-  <input type="hidden" name="item_name" value="'.$item_nom.'"/>
-  <input type="hidden" name="item_number" value="'.$item_numero.'"/>
-  <input type="hidden" name="amount" value="'.$item_prix.'"/>
-  <input type="hidden" name="currency_code" value="EUR"/>
-  <input type="hidden" name="no_note" value="1"/>
-  <input type="hidden" name="no_shipping" value="0"/>
-  <input type="hidden" name="lc" value="FR"/>
-  <input type="hidden" name="notify_url" value="'.$url_confirmation.'"/>
-  <input type="hidden" name="cancel_return" value="'.$url_cancel.'">
-  <input type="hidden" name="return" value="'.$url_retour.'">
-  <input  align="right" valign="center" type="image" alt="Paiement par Paypal" src=" https://www.paypal.com/fr_FR/i/bnr/horizontal_solution_PP.gif" border="0" name="submit" alt="Paiement sécurisé par paypal"/>
-  </form> ';
-?>
+		   <div class='form-row'>
+              <div class='col-xs-12 form-group required'>
+                <label class='control-label'>Nom sur la carte</label>
+                <input class='form-control' size='4' type='text'>
+              </div>
+            </div>
+           
+
+		   <div class='form-row'>
+              <div class='col-xs-12 form-group card required'>
+                <label class='control-label'>Numero de carte</label>
+                <input autocomplete='off' class='form-control card-number' size='20' type='text'>
+              </div>
+            </div>
+            <div class='form-row'>
+              <div class='col-xs-4 form-group cvc required'>
+                <label class='control-label'>CVC</label>
+                <input autocomplete='off' class='form-control card-cvc' placeholder='ex. 1234' size='4' type='text'>
+              </div>
+              <div class='col-xs-4 form-group expiration required'>
+                <label class='control-label'>Expiration</label>
+                <input class='form-control card-expiry-month' placeholder='MM' size='2' type='text'>
+              </div>
+              <div class='col-xs-4 form-group expiration required'>
+                <label class='control-label'> </label>
+                <input class='form-control card-expiry-year' placeholder='YYYY' size='4' type='text'>
+              </div>
+            </div>
+           
+
+
+		   <div class='form-row'>
+              <div class='col-md-12'>
+                <div class='form-control total btn btn-info'>
+                  Total:
+                  <span class='amount'>€150</span>
+                </div>
+              </div>
+            </div>
+            <div class='form-row'>
+              <div class='col-md-12 form-group'>
+                <button class='form-control btn btn-primary submit-button' type='submit'>Payer </button>
+              </div>
+            </div>
+            <div class='form-row'>
+              <div class='col-md-12 error form-group hide'>
+                <div class='alert-danger alert'>
+                  Corrigez les erreurs et recommencez
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div class='col-md-4'></div>
+    </div>
+</div>
+<?php include_once("footer.php") ?>
