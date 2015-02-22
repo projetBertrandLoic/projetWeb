7a16c31bef40143a06fc1cd7ae5dcb54c92efd04
gisselmann <flythe747@free.fr>
2015-02-22 14:24:01 +0100

    depalcement d un commentaire dans article .php

--- article.php
+++ article.php
@@ -29,7 +29,7 @@
 														<!-- -->
 														
 														
-														
+														<!--c'est l'endroit ou je rajoute le boutton ajouter panier-->
                                                         <button class="btn btn-primary btn-sm">Acheter</button>
                                                     </div>
                                                     <p>Morbi consectetur velit et est placerat volutpat. Aliquam lacus libero, iaculis sit amet ornare eu, bibendum convallis. Curabitur vel erat imperdiet, ultrices dui et, mattis arcu. Donec nisi velit, dignissim mollis erat vehicula, iaculis eleifend sem. Mauris non ultrices ante, id porta odio. Pellentesque eget egestas lorem. <a href="#">Nullam vitae</a> varius lorem, ac tristique justo. Cras placerat tempus pharetra. Class aptent taciti sociosqu ad litora torquent per conubia nostra, an inceptos himenaeos. Morbi et feugiat justo. <br><br>Maecenas et ante eu sem viverra faucibus eget nec est. Aenean non felis diam. Praesent malesuada, lectus vel elementum tincidunt, sapien dolor interdum lacus, vel suscipit enim augue vel purus. Ut in augue mass. Etiam a leo at eros vehicula mattis. Aenean nec fringilla neque <a href="#">eget vulputate mi</a>. Aenean tincidunt elit sollicitudin libero hendrerit feugiat. Nunc purus leo, sollicitudin et vulputate vitae, auctor a lacus. Fusce aliquet erat quis nisi adipiscing, id eleifend quam venenatis. Integer non sem quis dui dignissim lacinia et a massa.</p>
@@ -37,7 +37,7 @@
                                                 <div class="divide-line">
                                                 <img src="images/div-line.png" alt="" />
                                                 </div>
-												<!--c'est l'endroit ou je rajoute le boutton ajouter panier-->
+												
 										
                                         </div>
                                         <div class="col-md-3 col-md-offset-1">
