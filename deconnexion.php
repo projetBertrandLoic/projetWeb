cdf8f41d691ae4f2d80633c0eb407ad3f394455f
Bertrand Duthe <baekdoosixt@gmail.com>
2015-02-24 16:36:27 +0100

    re commit apres erreur

--- deconnexion.php
+++ deconnexion.php
@@ -6,15 +6,6 @@
 		session_destroy();
 		
 		?><div class="panel panel-success">Vous etes bien deconnecté</div>
-		
-		<div >
-			<form class="form-horizontal" action='index.php' method="POST">
-			</br></br>
-				<button class="btn btn-primary" type="submit">Retourner à l'acceuil</button>
-		</div>		
-		
-		
-		
 		<?php
 		header ("Refresh: 2;URL=index.php");
 		include_once('footer.php');
